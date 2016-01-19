7616e4dbe5ec7a53201e49227d2942ac86345b5b
Miguel <miguelon645@gmail.com>
2016-01-19 10:15:16 -0600

    Se reparo la funcion de respaldo

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -36,7 +36,7 @@ class UsuarioController extends Controller
 
     public function respaldo()
     {
-        define("BACKUP_PATH", "/var/www/html/");
+        define("BACKUP_PATH", "/var/www/html/financiero/respaldos/");
 
         $server_name   = "localhost";
         $username      = "root";
