b4b519bdbef71049e6bbb3644110d71f54f88c56
arturoMV <r2robsr@gmail.com>
2016-01-28 13:07:57 -0600

    respaldos de nuevo

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -41,13 +41,13 @@ class UsuarioController extends Controller
      */
     public function respaldo()
     {
-        define("BACKUP_PATH", "/var/www/html/financiero/respaldos/Illuminate");
+        define("BACKUP_PATH", "/var/www/html/financiero/public/respaldos/");
 
         $server_name   = "localhost";
         $username      = "root";
         $password      = "1234";
         $database_name = "presupuestooaf";
-        $date_string   = date("d-m-Y");
+        $date_string   = date("d-m-Y H:i:s");
 
         $cmd = "mysqldump --hex-blob --routines --skip-lock-tables --log-error=mysqldump_error.log -h {$server_name} -u {$username} -p{$password} {$database_name} > " . BACKUP_PATH . "Backup-{$date_string}_{$database_name}.sql";
 
